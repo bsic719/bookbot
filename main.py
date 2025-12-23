6909100fdb78eaba189d50dd9d3d850ce6d6e8ef
bsic719 <besic719@gmail.com>
2025-12-22 20:42:53 -0500

    updated main.py with a counter

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print('greetings boots')
+def get_book_text(file):
+    with open(file) as f:
+        return f.read()
+
+def word_counter(text):
+    word_list = text.split()
+
+    print(f"Found {len(word_list)} total words")
+
+def main():
+    book_path = 'books/frankenstein.txt'
+    texts = get_book_text('books/frankenstein.txt')
+    
+    word_counter(texts)
+
+if __name__ == '__main__':
+    main()
